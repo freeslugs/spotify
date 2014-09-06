2e07fcf2bc4f0eaa3644ab4bfba84487c16f3a52
freeslugs <giladpenn@gmail.com>
2014-09-06 13:03:06 -0400

    Initial Commit

---
+++ SpotifyHackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyHackathon
+//
+//  Created by Gilad Penn on 9/6/14.
+//  Copyright (c) 2014 512Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
