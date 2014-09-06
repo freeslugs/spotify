1e8316998c45f7b619b0a609b905918f0050dc9f
mimouncadosch <mimouncadosch@gmail.com>
2014-09-06 16:31:56 -0400

    FB SDK Set up

---
+++ SpotifyHackathon/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  SpotifyHackathon
+//
+//  Created by Mimoun Paul Cadosch Delmar on 9/6/14.
+//  Copyright (c) 2014 512Tech. All rights reserved.
+//
+
+#ifndef SpotifyHackathon_Bridge_Header_h
+#define SpotifyHackathon_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
