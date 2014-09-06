4dc779c89b9eb35f5e6bba80694517b650bb2660
mimouncadosch <mimouncadosch@gmail.com>
2014-09-06 18:15:53 -0400

    Committing

---
+++ SpotifyHackathon/FriendsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsViewController.swift
+//  SpotifyHackathon
+//
+//  Created by Mimoun Paul Cadosch Delmar on 9/6/14.
+//  Copyright (c) 2014 512Tech. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UITableViewController {
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        println("viewDidLoad")
+//    }
+    
+}
